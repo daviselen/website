81d97d51dd44a0ccf969488e57ce9a3e29f474c0
Alexander Bell <contact@alexanderbell.info>
2026-09-17 13:27:37 -0700

    Give StyledField a children escape hatch

--- src/design-system/components/Field.jsx
+++ src/design-system/components/Field.jsx
@@ -2,11 +2,15 @@ import { Field } from '@headlessui/react'
 import StyledLabel from './Label'
 import StyledInput from './Input'
 
-export default function StyledField({type, label}) {
+export default function StyledField({type, label, children}) {
     return (
         <Field className="flex flex-col gap-100">
-            <StyledLabel>{label}</StyledLabel>
-            <StyledInput type={type} name="full_name" />
+            {children ?? (
+                <>
+                    <StyledLabel>{label}</StyledLabel>
+                    <StyledInput type={type} name="full_name" />
+                </>
+            )}
         </Field>
     )
 }
